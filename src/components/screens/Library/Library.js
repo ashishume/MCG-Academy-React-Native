c7b6d703bf804425ea3aa2d4b36ce4bc5ce68dbe
Ashish <you@example.com>
2020-08-02 23:03:41 +0530

    library and my courses converted to hooks

--- src/components/screens/Library/Library.js
+++ src/components/screens/Library/Library.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component, Fragment, useEffect} from 'react';
 import {Text, View, Linking} from 'react-native';
 import {fetchAllLibrary} from '../../../store/actions/library';
 import {connect} from 'react-redux';
@@ -6,35 +6,37 @@ import LibraryCard from './LibraryCard';
 import TopHeader from '../../Shared/Header';
 import {ScrollView} from 'react-native-gesture-handler';
 
-class Library extends Component {
-  componentDidMount() {
-    this.props.fetchAllLibrary();
-  }
-  clickLibraryItem = (e) => {
+const Library = (props) => {
+  useEffect(() => {
+    const unsubscribe = props.navigation.addListener('focus', () => {
+      props.fetchAllLibrary();
+    });
+    return unsubscribe;
+  }, [props.navigation]);
+
+  const clickLibraryItem = (e) => {
     Linking.openURL(e.fileUrl);
   };
-  render() {
-    return (
-      <Fragment>
-        <TopHeader name="Library files" />
-        <View
-          style={{height: '100%', backgroundColor: '#fff', paddingBottom: 70}}>
-          <ScrollView scrollEventThrottle={25} indicatorStyle="black">
-            {this.props.library.map((value, i) => {
-              return (
-                <LibraryCard
-                  content={value}
-                  key={i}
-                  onClickLibrayItem={() => this.clickLibraryItem(value)}
-                />
-              );
-            })}
-          </ScrollView>
-        </View>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <TopHeader name="Library files" />
+      <View
+        style={{height: '100%', backgroundColor: '#fff', paddingBottom: 70}}>
+        <ScrollView scrollEventThrottle={25} indicatorStyle="black">
+          {props.library.map((value, i) => {
+            return (
+              <LibraryCard
+                content={value}
+                key={i}
+                onClickLibrayItem={() => clickLibraryItem(value)}
+              />
+            );
+          })}
+        </ScrollView>
+      </View>
+    </Fragment>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
--- src/components/screens/MyCourses/MyCourses.js
+++ src/components/screens/MyCourses/MyCourses.js
@@ -1,53 +1,58 @@
-import React, {Component, Fragment} from 'react';
-import {Text, View, SafeAreaView, ScrollView, ToastAndroid} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, SafeAreaView, ScrollView, ToastAndroid} from 'react-native';
 import TopHeader from '../../Shared/Header';
 import CourseCardListItem from './CourseCardListItem';
 import {connect} from 'react-redux';
 import {fetchMyCourses, fetchMyCourseIds} from '../../../store/actions/courses';
 import AsyncStorage from '@react-native-community/async-storage';
 
-class MyCourses extends Component {
-  async componentDidMount() {
-    try {
-      const userId = await AsyncStorage.getItem('userId');
-      await this.props.fetchMyCourseIds(userId);
-      if (this.props.myCoursesIds) {
-        const body = {
-          courses: this.props.myCoursesIds,
-        };
-        await this.props.fetchMyCourses(body);
+const MyCourses = (props) => {
+  useEffect(() => {
+    const fetchCourseData = async () => {
+      try {
+        const userId = await AsyncStorage.getItem('userId');
+        await props.fetchMyCourseIds(userId);
+        if (props.myCoursesIds) {
+          const body = {
+            courses: props.myCoursesIds,
+          };
+          await props.fetchMyCourses(body);
+        }
+      } catch (e) {
+        ToastAndroid.show('Something went wrong', ToastAndroid.SHORT);
       }
-    } catch (e) {
-      ToastAndroid.show('Something went wrong', ToastAndroid.SHORT);
-    }
-  }
-  courseEventHandler = (value) => {
-    this.props.navigation.navigate('CourseContent', value);
+    };
+    const unsubscribe = props.navigation.addListener('focus', () => {
+      fetchCourseData();
+    });
+    return unsubscribe;
+  }, [props.navigation]);
+
+  const courseEventHandler = (value) => {
+    props.navigation.navigate('CourseContent', value);
   };
 
-  renderData = () => {
-    return this.props.courses.map((value, i) => {
+  const renderData = () => {
+    return props.courses.map((value, i) => {
       return (
         <CourseCardListItem
           key={i}
-          onClickCourseItem={() => this.courseEventHandler(value)}
+          onClickCourseItem={() => courseEventHandler(value)}
           content={value}
         />
       );
     });
   };
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <TopHeader name="My Courses" />
-        <SafeAreaView
-          style={{flex: 1, backgroundColor: '#fff', paddingBottom: 5}}>
-          <ScrollView style={{flex: 1}}>{this.renderData()}</ScrollView>
-        </SafeAreaView>
-      </View>
-    );
-  }
-}
+  return (
+    <View style={{flex: 1}}>
+      <TopHeader name="My Courses" />
+      <SafeAreaView
+        style={{flex: 1, backgroundColor: '#fff', paddingBottom: 5}}>
+        <ScrollView style={{flex: 1}}>{renderData()}</ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+};
 const mapStateToProps = (state) => {
   return {
     myCoursesIds: state.courses.myCourseIds,
