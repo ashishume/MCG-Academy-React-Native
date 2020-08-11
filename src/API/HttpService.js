1a7e699bea9dd9c7b8de00ae323af634eda1a47a
Ashish <you@example.com>
2020-08-11 19:47:38 +0530

    url changed

--- src/API/HttpService.js
+++ src/API/HttpService.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const http = axios.create({
-  // baseURL: 'http://192.168.1.47:5000/',    //jio router wifi
-  // baseURL: 'http://192.168.43.120:5000/', //samsung wifi
   baseURL: 'https://mcg-academy.herokuapp.com/',
 });
 
