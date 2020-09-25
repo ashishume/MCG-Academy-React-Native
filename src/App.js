d42fe7b4eb02107e8b1665f4aa99491237d3f55a
Ashish <you@example.com>
2020-09-26 00:37:21 +0530

    fcm integrated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React, {Fragment, Component} from 'react';
+import React, {Fragment, useEffect, Component} from 'react';
 import MainRouting from './Routes/MainRouting';
 import LoaderComponent from './components/LoaderComponent';
 import {StatusBar} from 'react-native';
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <StatusBar backgroundColor="black" />
-        <MainRouting />
-        <LoaderComponent />
-      </Fragment>
-    );
-  }
-}
+import {fcmService} from './Services/FCMService';
+import {localNotificationService} from './Services/LocalNotificationService';
+const App = () => {
+  useEffect(() => {
+    // fcmService.registerAppWithFCM()
+    fcmService.register(onRegister, onNotification, onOpenNotification);
+    localNotificationService.configure(onOpenNotification);
+
+    function onRegister(token) {
+      console.log('[App] onRegister: ', token);
+    }
+
+    function onNotification(notify) {
+      console.log('[App] onNotification: ', notify);
+      const options = {
+        soundName: 'default',
+        playSound: true, //,
+        // largeIcon: 'ic_launcher', // add icon large for Android (Link: app/src/main/mipmap)
+        // smallIcon: 'ic_launcher' // add icon small for Android (Link: app/src/main/mipmap)
+      };
+      localNotificationService.showNotification(
+        0,
+        notify.title,
+        notify.body,
+        notify,
+        options,
+      );
+    }
+
+    function onOpenNotification(notify) {
+      console.log('[App] onOpenNotification: ', notify);
+      // alert('Open Notification: ' + notify.body);
+    }
+
+    return () => {
+      console.log('[App] unRegister');
+      fcmService.unRegister();
+      localNotificationService.unregister();
+    };
+  }, []);
+  return (
+    <Fragment>
+      <StatusBar backgroundColor="black" />
+      <MainRouting />
+      <LoaderComponent />
+    </Fragment>
+  );
+};
 export default App;
