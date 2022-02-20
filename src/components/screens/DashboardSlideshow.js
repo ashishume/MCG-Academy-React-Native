f4bdef39741f42d9e191f751d2dd8371942e27a4
Ashish Debnath <ashish.debnath@nineleaps.com>
2022-02-20 14:50:35 +0530

    fix: carousel image click added

--- src/components/screens/DashboardSlideshow.js
+++ src/components/screens/DashboardSlideshow.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
-import {Image, Dimensions, View} from 'react-native';
-import {connect} from 'react-redux';
+import {Image, Dimensions, View, TouchableOpacity} from 'react-native';
+import {connect, useDispatch, useSelector} from 'react-redux';
 import {fetchAllImages} from '../../store/actions/images';
 import Carousel from 'react-native-snap-carousel';
 
@@ -11,24 +11,34 @@ const sliderWidth = Dimensions.get('window').width;
 const itemWidth = slideWidth + horizontalMargin * 2;
 const itemHeight = 200;
 
-const DashboardSlideshow = (props) => {
+const DashboardSlideshow = ({navigation}) => {
+  const images = useSelector((state) => state.images.images);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    props.fetchAllImages();
+    dispatch(fetchAllImages());
   }, []);
+
   const _renderItem = ({item, index}) => {
     return (
-      <View>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={(e) => {
+          navigation.navigate('course', {
+            courseId: item?.course,
+          });
+        }}>
         <Image
           source={{uri: item.imageUrl}}
-          style={{width: '100%', height: 200, resizeMode: 'cover'}}
+          style={{width: '100%', height: 200, resizeMode: 'cover', zIndex: 1}}
         />
-      </View>
+      </TouchableOpacity>
     );
   };
   return (
     <View style={{marginVertical: 5, flex: 1}}>
       <Carousel
-        data={props.images}
+        data={images}
         renderItem={_renderItem}
         sliderWidth={sliderWidth}
         itemWidth={itemWidth}
@@ -36,9 +46,5 @@ const DashboardSlideshow = (props) => {
     </View>
   );
 };
-const mapStateToProps = (state) => {
-  return {
-    images: state.images.images,
-  };
-};
-export default connect(mapStateToProps, {fetchAllImages})(DashboardSlideshow);
+
+export default DashboardSlideshow;
